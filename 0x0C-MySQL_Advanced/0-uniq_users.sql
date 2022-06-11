78745b4bba698eac97b7af2f6c2e529fb688ae88
Jaime Martinez <58230371+jemn21819@users.noreply.github.com>
2022-06-11 19:57:31 -0400

    Create 0-uniq_users.sql

---
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- This file creates a users table
+-- users table has 3 fields: id, email, name
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
